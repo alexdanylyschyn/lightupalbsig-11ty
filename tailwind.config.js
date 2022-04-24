1fd178f58c4feef1eb794ec6ef32b2ae716f9370
Alexander Danylyschyn <alex@dyyy.dev>
2022-04-24 18:21:49 +0200

    feat: basic content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,20 @@
 module.exports = {
 	theme: {
 		fontFamily: {
-			'sans': ['Inter', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+			sans: ["Inter", "ui-sans-serif", "system-ui", "sans-serif"],
+			serif: [
+				"CMU Serif",
+				"ui-serif",
+				"Georgia",
+				"Cambria",
+				"Times New Roman",
+				"Times",
+				"serif",
+			],
+			display: ["MoonTime"],
 		},
-		extend: {
-			colors: {
-				'ws-blue': {
-					100: '#E6F0FF',
-					200: '#BFDAFF',
-					300: '#99C3FF',
-					400: '#4D97FE',
-					500: '#006AFE',
-					600: '#005FE5',
-					700: '#004098',
-					800: '#003072',
-					900: '#00204C',
-				},
-			}
-		},
+		extend: {},
 	},
 	plugins: [],
-	content: ['src/**/*.njk', 'src/**/*.js']
-}
+	content: ["src/**/*.njk", "src/**/*.js"],
+};
